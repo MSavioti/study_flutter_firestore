8c3b776217ce1658dfe8f30aedb903a6239a338d
angelo-squadra <angelo.savioti@squadra.com.br>
2022-04-22 13:40:37 -0300

    added route to purchase details page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:study_flutter_firestore/app/pages/add_purchase/add_purchase_page.dart';
 import 'package:study_flutter_firestore/app/pages/home/home_page.dart';
+import 'package:study_flutter_firestore/app/pages/purchase_details/purchase_details_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -16,6 +17,7 @@ class App extends StatelessWidget {
       routes: {
         '/': (context) => const HomePage(),
         '/add_purchase': (context) => const AddPurchasePage(),
+        '/purchase_details': (context) => const PurchaseDetailsPage(),
       },
     );
   }
